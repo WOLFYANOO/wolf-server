7ecb861195b259eb05608d70428075c4bfc73e94
mahmoud khoder <80870331+WOLFYANOO@users.noreply.github.com>
2025-06-11 12:08:45 +0300

    main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
+  // 
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
   app.enableCors({
